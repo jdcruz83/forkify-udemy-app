5df28cef405b74425e8b9a6db56ee88cb56850bd
jdcruz83 <jdcruz83@hotmail.com>
2023-02-15 00:01:41 +0000

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -145,6 +145,10 @@ const controlAddRecipe = async function (newRecipe) {
     }
 };
 
+const newFeature = function () {
+    console.log('Welcome to the application');
+};
+
 const init = function () {
     bookmarksView.addHandlerRender(controlBookmarks);
     recipeView.addHandlerRender(controlRecipes);
@@ -153,6 +157,7 @@ const init = function () {
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
     addRecipeView.addHandlerUpload(controlAddRecipe);
+    newFeature();
 };
 
 init();
